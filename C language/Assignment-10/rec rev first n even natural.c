60108f6f74366563e818faf31288bba27fcf1748
Abhishek Patel <itsabhishekpatel01@gmail.com>
2025-01-05 09:55:24 +0530

    second commit

---
+++ C language/Assignment-10/rec rev first n even natural.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int first_nat(int);
+
+main()
+{
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    first_nat(2*n);
+
+    return 0;
+}
+
+int first_nat(int a)
+{
+    if (a==0)
+    return 1;
+    if(a%2==0)
+    printf("%d\n",a);
+    first_nat(a-1);
+}
